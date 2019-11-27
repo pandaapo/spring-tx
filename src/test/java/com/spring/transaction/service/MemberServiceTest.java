0d37eb83c61c0af25d48aecbcd678e00acd8113d
pandaapo <1052156701@qq.com>
2019-11-27 22:59:53 +0800

    一个基本的Spring Jdbctemplate Demo

---
+++ src/test/java/com/spring/transaction/service/MemberServiceTest.java
@@ -0,0 +1,44 @@
+package com.spring.transaction.service;
+
+import com.alibaba.fastjson.JSON;
+import com.spring.transaction.entity.Member;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.sql.Connection;
+import java.util.List;
+
+@ContextConfiguration(locations = {"classpath*:application-context.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    @Ignore
+    public void queryAll() {
+        try {
+            List<Member> list = memberService.queryAll();
+            System.out.println(JSON.toJSONString(list, true));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+//    @Ignore
+    public void testRemove() {
+        try {
+            boolean r = memberService.remove(1L);
+            System.out.println(r);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
